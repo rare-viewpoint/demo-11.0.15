4e71c8b356c3c4a85c7ae7c45a568df9b014575f
shmmmao <107337126+shmmmao@users.noreply.github.com>
2022-06-12 22:59:45 +0800

    test

---
+++ src/demo/collection/DemoTest.java
@@ -0,0 +1,16 @@
+package demo.collection;
+
+import java.sql.SQLOutput;
+
+/**
+ * Author shm
+ * Desc
+ * Date 2022/6/12 22:57
+ */
+public class DemoTest {
+
+    public static void main(String[] args) {
+        System.out.println("This is a Demo Test");
+    }
+
+}
